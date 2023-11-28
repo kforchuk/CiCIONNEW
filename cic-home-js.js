7e8ca5ceebf265bb0721ba5ee6ccba7e4f1ccbb5
kforchuk <89806985+kforchuk@users.noreply.github.com>
2023-11-28 15:31:23 -0700

    Add files via upload

---
+++ cic-home-js.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    var x = document.getElementById("options-dropdown");
+    if (x.style.display == "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
